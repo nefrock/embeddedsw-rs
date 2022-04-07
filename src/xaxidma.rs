79b8c6b501667bc9cde9fe8c434fb05149adc179
kadu-v <kazuonsunday@yahoo.co.jp>
2022-04-07 18:02:27 +0900

    Add: doc comments for xaxidma.rs

--- src/xaxidma.rs
+++ src/xaxidma.rs
@@ -1,6 +1,9 @@
 extern crate embeddedsw_sys;
+use core::mem::MaybeUninit;
+
 use embeddedsw_sys as esys;
 
+/// Enumurates for DMA driver errors
 #[derive(Debug)]
 pub enum DmaError {
     InvalidParam,
@@ -12,18 +15,69 @@ pub enum DmaError {
     ConfigInit,
 }
 
+/// Enumerates for DMA directions
+///
+/// DMAToDevice: PS  -> DDR
+/// DeviceToDMA: DDR -> PS
+///
+///                 +------+
+///              +- |  PS  | --+
+///              |  +------+   |
+/// DeviceToDMA  |             |  DMAToDevice
+///              |  +-------+  |
+///              +->|  DDR  |<-+
+///                 +-------+     
+///                     ^          
+///                     |
+///                     v
+///                 +------+
+///                 |  PL  |
+///                 +------+
 #[derive(Debug)]
 pub enum DmaDirection {
     DMAToDevice,
     DeviceToDMA,
 }
 
+/// The configuration structure for AXI DMA engine.
+///
+/// # Example
+/// ```
+/// #
+/// #
+/// # #![no_std]
+/// # #![no_main]
+/// # #![feature(start)]
+/// # extern crate alloc;
+/// # extern crate embeddedsw_rs;
+/// # use embeddedsw_rs as xemb;
+/// # use xemb::{ println, xaxidma::{ self, XAxiDma, XAxiDmaXonfig }, raw::* };
+/// # [panic_handler]
+/// # fn panic(info: &core::panic::PanicInfo<'_>) -> ! {
+/// #    println!("{}", info);
+/// #    loop {}
+/// # }
+/// #
+/// # #[no_mangle]
+/// # #[start]
+/// # fn main() {
+///    // XPAR_AXI_DMA_0_DEVICE_ID is defined in parameters.h, so bindgen generates constants.
+///    let mut axi_dma_config = XAxiDmaConfig::lookup_config(raw::XPAR_AXI_DMA_0_DEVICE_ID).unwarap();
+///    
+/// # }
+/// ```
 #[repr(C)]
 pub struct XAxiDmaConfig {
     config: *mut esys::XAxiDma_Config,
 }
 
 impl XAxiDmaConfig {
+    /// Look up the hardware configuration for XAxiDma instance.
+    ///
+    /// # Errors
+    /// If this function cannot find device configuration,
+    /// it returns DmaError variants for configuration initalize.
+    ///
     pub fn lookup_config(
         id: u32,
     ) -> Result<Self, DmaError> {
@@ -36,18 +90,74 @@ impl XAxiDmaConfig {
     }
 }
 
+/// The device instance structs for XAxiDma.
+///
+///
+/// # Safety
+/// An instance must be allocated for each instance in use.
+///
+/// # Example
+/// ```
+/// #
+/// #
+/// #![no_std]
+/// #![no_main]
+/// #![feature(start)]
+/// # extern crate alloc;
+/// extern crate embeddedsw_rs;
+/// use embeddedsw_rs as xemb;
+/// use xemb::{ println, xaxidma::{ self, XAxiDma, XAxiDmaXonfig }, raw::* };
+/// # [panic_handler]
+/// # fn panic(info: &core::panic::PanicInfo<'_>) -> ! {
+/// #    println!("{}", info);
+/// #    loop {}
+/// # }
+/// #
+///
+/// #[no_mangle]
+/// #[start]
+/// fn main(_argc: isize, _argv: *const *const u8) -> isize {
+///    // XPAR_AXI_DMA_0_DEVICE_ID is defined in parameters.h, so bindgen generates constants.
+///    let mut axi_dma_config = XAxiDmaConfig::lookup_config(raw::XPAR_AXI_DMA_0_DEVICE_ID).unwarap();
+///    
+///    // initalize DMA instance
+///    let mut axi_dma = MaybeUninit::<XAxiDma>::uninit();
+///    XAxiDma::cfg_initialize(&mut axi_dma, &mut axi_dma_config).unwrap();
+///    let mut axi_dma = axi_dma.assume_init();
+///    
+///    // disable IRQ interrupt
+///    axi_dma.axi_irq_interrupt_disable(DMAToDevice);
+///    axi_dma.axi_irq_interrupt_disable(DeviceToDMA);
+///
+///    // flush DCache
+///    xaxidma::xil_dcach_flush_range(dma_base_addr as isize, dma_size);
+///    
+///    // simple DMA transfer
+///    axi_dma.axi_simple_transfer(dma_base_addr, dma_size, xaxidma::DMAToDevice).unwrap();
+///
+///    while axi_dma.busy() {
+///       // wait here
+///    }
+///
+///    return 0;
+/// }
+/// ```
 #[repr(C)]
 pub struct XAxiDma {
-    pub axi_dma: esys::XAxiDma,
+    inner: esys::XAxiDma,
 }
 
 impl XAxiDma {
-    pub unsafe fn cfg_initialize(
-        &mut self,
+    /// This function inilializes a DMA instance.
+    ///
+    /// # Safety  
+    /// This function must be called before using DMA instances.
+    pub fn cfg_initialize(
+        xaxidma: &mut MaybeUninit<XAxiDma>,
         config: &mut XAxiDmaConfig,
     ) -> Result<(), DmaError> {
         match esys::XAxiDma_CfgInitialize(
-            &mut self.axi_dma as *mut _,
+            self.as_mut_ptr() as *mut XAxiDma,
             config.config,
         ) as u32
         {
@@ -59,12 +169,15 @@ impl XAxiDma {
         }
     }
 
+    /// This function resets both TX and RX channels of a DMA instance.
+    /// If you reset one channel by this function, the whole AXI DMA instance is reseted.
     pub fn reset(&mut self) {
         unsafe {
             esys::XAxiDma_Reset(&mut self.axi_dma as *mut _)
         }
     }
 
+    /// This function checks whether rest is done.
     pub fn reset_is_done(&mut self) -> bool {
         unsafe {
             match esys::XAxiDma_ResetIsDone(
@@ -76,6 +189,13 @@ impl XAxiDma {
         }
     }
 
+    /// This function pauses DMA transaction on both chennels.
+    ///
+    /// # Error
+    /// - If this function is called before initalizing XAxiDMA structs,
+    ///   it returns DmaError variants for NotSGDMA.
+    /// - If this function cannot pause AXI DMA transactions due to unknown error,
+    ///   it returns DMAError variants for Unknown.
     pub fn pause(&mut self) -> Result<(), DmaError> {
         unsafe {
             match esys::XAxiDma_Pause(
@@ -91,6 +211,13 @@ impl XAxiDma {
         }
     }
 
+    /// This function resumes DMA transactions on both channels.
+    ///
+    /// # Error
+    /// - If this function is called before initalizing XAxiDMA structs,
+    ///   it returns DmaError variants for NotSGDMA.
+    /// - If this function cannot pause AXI DMA transactions due to unknown error,
+    ///   it returns DMAError variants for Unknown.
     pub fn resume(&mut self) -> Result<(), DmaError> {
         unsafe {
             match esys::XAxiDma_Resume(
@@ -109,6 +236,7 @@ impl XAxiDma {
         }
     }
 
+    /// This function checks whether the DMA instance  is busy.
     pub fn busy(
         &mut self,
         direction: DmaDirection,
@@ -133,6 +261,12 @@ impl XAxiDma {
         }
     }
 
+    /// This function does one simle transfer submisson.
+    ///
+    ///
+    /// # Error
+    /// - If DMA instance is busy, it cannot submit.
+    /// - If DMA instance is in SG mode, it cannot submit.
     pub fn simple_transfer(
         &mut self,
         buff_addr: usize,
@@ -166,6 +300,7 @@ impl XAxiDma {
         }
     }
 
+    /// This function runs a self-test on the driver/device.
     pub fn self_test(&mut self) -> Result<(), DmaError> {
         unsafe {
             match esys::XAxiDma_Selftest(
@@ -195,6 +330,7 @@ impl XAxiDma {
         )
     }
 
+    /// This function enables IRQ interrupt.
     pub fn irq_interrupt_enable(
         &self,
         direction: DmaDirection,
@@ -222,6 +358,7 @@ impl XAxiDma {
         }
     }
 
+    /// This function disables IRQ interrupt.
     pub fn irq_interrupt_disable(
         &self,
         direction: DmaDirection,
@@ -254,6 +391,7 @@ impl XAxiDma {
 // Public functions
 //-------------------------------------------------------------------------------------------------
 
+/// This function flishs DCache.
 pub fn xil_dcache_flush_range(addr: isize, length: u32) {
     unsafe { esys::Xil_DCacheFlushRange(addr, length) }
 }
